1b3796f192e63e257114fe674c455a51ad0dc174
Rafid13iit <rafidssrr.6767@gmail.com>
2023-11-11 18:15:31 +0600

    files are updated

--- AQI Web App/script.js
+++ AQI Web App/script.js
@@ -14,13 +14,33 @@ const airQuality = {
   },
   displayAirQuality: function (data) {
     const { city, state, country, current } = data.data;
-    const { aqi, ic } = current;
-    document.querySelector(".city").innerText = "Air Quality in " + city + " " + state + " " + country;
+    const { aqius, ic } = current.pollution; // Use aqius for the AQI value
+    const airQualityLevel = this.getAirQualityLevel(aqius);
+    
+    document.querySelector(".city").innerText = "Air Quality in " + city + ", " + country;
     document.querySelector(".description").innerText = ic;
-    document.querySelector(".index").innerText = "AQI: " + aqi;
+    document.querySelector(".index").innerText = "AQI: " + aqius; // Display the AQI value
     document.querySelector(".air-quality").classList.remove("loading");
     document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + city + "')";
+    
+    // Add a class to change background color based on air quality level
+    document.querySelector(".card").classList.add(airQualityLevel);
+  },
+  getAirQualityLevel: function (aqi) {
+    if (aqi <= 50) {
+      return "good";
+    } else if (aqi <= 100) {
+      return "moderate";
+    } else if (aqi <= 150) {
+      return "unhealthy-for-sensitive-groups";
+    } else if (aqi <= 200) {
+      return "unhealthy";
+    } else if (aqi <= 300) {
+      return "very-unhealthy";
+    } else {
+      return "hazardous";
+    }
   },
   search: function () {
     const country = document.getElementById("country").value;
